0929febeec45f1b81128fe0d396812f4a0b4578b
Furkan <furkan.ovut@virgosol.com>
2023-10-25 10:32:39 +0300

    firs cucumber framework

---
+++ src/test/java/Features/Login.feature
@@ -0,0 +1,5 @@
+Feature: Login Functionality
+  In order to do internet banking
+  As a valid Para Bank customer
+  I want to login successfully
+
---
+++ src/test/java/StepDefination/Steps.java
@@ -0,0 +1,18 @@
+package StepDefination;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Steps {
+
+
+
+}
